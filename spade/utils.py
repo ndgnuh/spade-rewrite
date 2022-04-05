8a076205dec225d59e50c1123ce81c4931a6d069
Nguyễn Đức Hùng <hungnd@hiworld.com.vn>
2022-04-05 17:00:32 +0700

    move score functions to utils

--- spade/utils.py
+++ spade/utils.py
@@ -1,7 +1,10 @@
-from pprint import pformat
 import os
 import importlib
 import yaml
+import numpy as np
+import torch
+
+# CONFIG RELATED
 
 
 class AttrDict(dict):
@@ -30,3 +33,41 @@ def load_code_path(path):
     name = name[1:]  # Remove '.' in the extension
     module = importlib.import_module(module_path)
     return getattr(module, name)
+
+
+# SCORE RELATED
+
+def ensure_numpy(x):
+    # Convert to numpy, or just stay as numpy
+    if isinstance(x, torch.Tensor):
+        return x.cpu().numpy()
+    if isinstance(x, np.ndarray):
+        return x
+    return np.array(x)
+
+
+def ensure_nz(x):
+    # Ensure x is not zero
+    return x + 1e-6 if x == 0 else x
+
+
+def scores(label, pr):
+    """
+    Return metrics between binary prediction and label
+    - accurary
+    - precision
+    - recall
+    - f1
+    """
+    pr = ensure_numpy(pr)
+    label = ensure_numpy(label)
+    tp = ((pr == 1) * (label == 1)).sum()
+    tn = ((pr == 0) * (label == 0)).sum()
+    fp = ((pr == 1) * (label == 0)).sum()
+    fn = ((pr == 0) * (label == 1)).sum()
+    return AttrDict(
+        accuracy=(tp + tn) / ensure_nz(tp + tn + fp + fn),
+        precision=tp / ensure_nz(tp + fp),
+        recall=tp / ensure_nz(tp + fn),
+        f1=2 * tp / ensure_nz(2 * tp + fp + fn),
+    )
