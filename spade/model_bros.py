aae73ee32d5708e3f87467598a29f0c5f7bf3047
Nguyễn Đức Hùng <hungnd@hiworld.com.vn>
2022-04-12 15:24:46 +0700

    bros model

---
+++ spade/model_bros.py
@@ -0,0 +1,391 @@
+import numpy as np
+from torch import nn
+from transformers import AutoModel, AutoTokenizer, BatchEncoding
+from torch.utils.data import Dataset, DataLoader
+from dataclasses import dataclass
+from typing import Optional
+from . import graph_stuff as G
+import torch
+import json
+from argparse import Namespace
+from functools import reduce
+from bros.bros import BrosModel
+
+
+def batch_consine_sim(batch):
+    score = torch.einsum("bih,bjh->bij", batch, batch)
+    inv_norm = 1 / torch.norm(batch, dim=-1)
+    return torch.einsum("bij,bi,bj->bij", score, inv_norm, inv_norm)
+
+
+def tensorize(x):
+    try:
+        return torch.tensor(np.array(x))
+    except Exception:
+        return torch.tensor(x)
+
+
+def true_length(mask):
+    batch = len(mask.shape) == 2
+    if batch:
+        return [true_length(m) for m in mask]
+
+    return torch.where(mask == 1)[-1][-1]
+
+
+class Transpose(nn.Module):
+    def __init__(self, dim_a, dim_b):
+        super().__init__()
+        self.dim_a = dim_a
+        self.dim_b = dim_b
+
+    def forward(self, x):
+        return x.transpose(self.dim_a, self.dim_b)
+
+
+class RelationTagger(nn.Module):
+    def __init__(self, n_fields, hidden_size, head_p_dropout=0.1):
+        super().__init__()
+        self.head = nn.Linear(hidden_size, hidden_size)
+        self.tail = nn.Linear(hidden_size, hidden_size)
+        self.field_embeddings = nn.Parameter(
+            torch.rand(1, n_fields, hidden_size))
+        self.W_label_0 = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.W_label_1 = nn.Linear(hidden_size, hidden_size, bias=False)
+
+    def forward(self, enc):
+
+        enc_head = self.head(enc)
+        enc_tail = self.tail(enc)
+
+        batch_size = enc_tail.size(0)
+        field_embeddings = self.field_embeddings.expand(batch_size, -1, -1)
+        enc_head = torch.cat([field_embeddings, enc_head], dim=1)
+
+        score_0 = torch.matmul(
+            enc_head, self.W_label_0(enc_tail).transpose(1, 2))
+        score_1 = torch.matmul(
+            enc_head, self.W_label_1(enc_tail).transpose(1, 2))
+
+        score = torch.cat(
+            [
+                score_0.unsqueeze(1),
+                score_1.unsqueeze(1),
+            ],
+            dim=1,
+        )
+        return score
+
+
+def partially_from_pretrained(config, model_name, **kwargs):
+    pretrain = AutoModel.from_pretrained(model_name, **kwargs)
+    model = type(pretrain)(config)
+    pretrain_sd = pretrain.state_dict()
+    for (k, v) in model.named_parameters():
+        if k not in pretrain_sd:
+            continue
+        if pretrain_sd[k].data.shape == v.shape:
+            v.data = pretrain_sd[k].data
+
+    return model
+
+
+def normalize_box(box, width, height):
+    bbox = tensorize(box)
+    bbox[[0, 2, 4, 6]] = bbox[[0, 2, 4, 6]] / width
+    bbox[[1, 3, 5, 7]] = bbox[[1, 3, 5, 7]] / height
+    return bbox
+
+
+def poly_to_box(poly):
+    # x = [p[0] for p in poly]
+    # y = [p[1] for p in poly]
+    # return [min(x), min(y), max(x), max(y)]
+    return reduce(lambda x, y: x + y, poly, [])
+
+
+def parse_input(
+    image,
+    words,
+    actual_boxes,
+    tokenizer,
+    config,
+    label,
+    fields,
+    cls_token_box=[0] * 8,
+    sep_token_box=None,
+    pad_token_box=[0] * 8,
+):
+    width, height = image.size
+    if sep_token_box is None:
+        sep_token_box = [width, height] * 4
+    boxes = actual_boxes
+    label = tensorize(label)
+    token_map = G.map_token(tokenizer, words, offset=len(fields))
+    rel_s = tensorize(label[0])
+    rel_g = tensorize(label[1])
+    token_rel_s = G.expand(rel_s, token_map, lh2ft=True,
+                           in_tail=True, in_head=True)
+    token_rel_g = G.expand(rel_g, token_map, fh2ft=True)
+    label = torch.cat(
+        [token_rel_s.unsqueeze(0), token_rel_g.unsqueeze(0)],
+        dim=0,
+    )
+
+    tokens = []
+    token_boxes = []
+    actual_bboxes = []  # we use an extra b because actual_boxes is already used
+    token_actual_boxes = []
+    are_box_first_tokens = []
+    for word, box, actual_bbox in zip(words, boxes, actual_boxes):
+        word_tokens = tokenizer.tokenize(word)
+        tokens.extend(word_tokens)
+        token_boxes.extend([box] * len(word_tokens))
+        actual_bboxes.extend([actual_bbox] * len(word_tokens))
+        token_actual_boxes.extend([actual_bbox] * len(word_tokens))
+        are_box_first_tokens.extend([1] + [0] * (len(word_tokens) - 1))
+
+    # Truncation: account for [CLS] and [SEP] with "- 2".
+    special_tokens_count = 2
+    true_length = config.max_position_embeddings - special_tokens_count
+    if len(tokens) > true_length:
+        tokens = tokens[:true_length]
+        token_boxes = token_boxes[:true_length]
+        token_actual_boxes = token_actual_boxes[:true_length]
+        actual_bboxes = actual_bboxes[:true_length]
+        are_box_first_tokens = are_box_first_tokens[:true_length]
+        label = label[:, : (len(fields) + true_length), :true_length]
+
+    # add [SEP] token, with corresponding token boxes and actual boxes
+    tokens += [tokenizer.sep_token]
+    token_boxes += [sep_token_box]
+    actual_bboxes += [[0, 0, width, height]]
+    token_actual_boxes += [[0, 0, width, height]]
+    are_box_first_tokens += [1]
+    # use labels for auxilary result
+    n, i, j = label.shape
+    labels = torch.zeros((n, i + 1, j + 1), dtype=label.dtype)
+    labels[:, :i, :j] = label
+    label = labels
+
+    segment_ids = [0] * len(tokens)
+
+    # print("----")
+    # edge_0 = []
+    # for (i, j) in zip(*torch.where(token_rel_s)):
+    #     l = [(fields + tokens)[i], tokens[j]]
+    #     print(l)
+    #     edge_0.append(" -> ".join(l))
+
+    # next: [CLS] token
+    tokens = [tokenizer.cls_token] + tokens
+    token_boxes = [cls_token_box] + token_boxes
+    actual_bboxes = [[0, 0, width, height]] + actual_bboxes
+    token_actual_boxes = [[0, 0, width, height]] + token_actual_boxes
+    segment_ids = [1] + segment_ids
+    are_box_first_tokens = [2] + are_box_first_tokens
+    # This is tricky because cls need to be inserted
+    # after the labels
+    nfields = len(fields)
+    top_half = label[:, :nfields, :]
+    bottom_half = label[:, nfields:, :]
+    n, i, j = label.shape
+    new_label = torch.zeros(n, i + 1, j + 1, dtype=label.dtype)
+    new_label[:, :nfields, 1:] = top_half
+    new_label[:, (nfields + 1):, 1:] = bottom_half
+    label = new_label
+
+    #     print("----")
+    #     print("AFter CLS")
+    #     edge_1 = []
+    #     for (i, j) in zip(*torch.where(label[0])):
+    #         l = [(fields + tokens)[i], tokens[j]]
+    #         print(l)
+    #         edge_1.append(" -> ".join(l))
+    #     print("----")
+
+    #     for (i, j) in zip(edge_0, edge_1):
+    #         print(i, j, i == j)
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    # The mask has 1 for real tokens and 0 for padding tokens. Only real
+    # tokens are attended to.
+    input_mask = [1] * len(input_ids)
+
+    # Zero-pad up to the sequence length.
+    padding_length = config.max_position_embeddings - len(input_ids)
+    input_ids += [tokenizer.pad_token_id] * padding_length
+    input_mask += [0] * padding_length
+    segment_ids += [tokenizer.pad_token_id] * padding_length
+    token_boxes += [pad_token_box] * padding_length
+    token_actual_boxes += [pad_token_box] * padding_length
+    are_box_first_tokens += [3] * padding_length
+
+    assert len(input_ids) == config.max_position_embeddings
+    assert len(input_mask) == config.max_position_embeddings
+    assert len(segment_ids) == config.max_position_embeddings
+    assert len(token_boxes) == config.max_position_embeddings
+    assert len(token_actual_boxes) == config.max_position_embeddings
+    assert len(are_box_first_tokens) == config.max_position_embeddings
+
+    # Label parsing
+    n, i, j = label.shape
+    labels = torch.zeros(
+        n,
+        config.max_position_embeddings + len(fields),
+        config.max_position_embeddings,
+        dtype=label.dtype,
+    )
+    labels[:, :i, :j] = label
+    label = labels
+
+    # assert itc_labels.shape[0] == config.max_position_embeddings
+    # assert stc_labels.shape[1] == config.max_position_embeddings
+    # assert stc_labels.shape[2] == config.max_position_embeddings
+
+    # labels = torch.cat(
+    #     [torch.zeros(labels.shape[0], 1, config.max_position_embeddings), labels],
+    #     dim=1,
+    # )
+
+    token_boxes = torch.tensor(token_boxes, dtype=torch.float).unsqueeze(0)
+    token_boxes[:, :, [0, 2, 4, 6]] = token_boxes[:, :, [0, 2, 4, 6]] / width
+    token_boxes[:, :, [1, 3, 5, 7]] = token_boxes[:, :, [1, 3, 5, 7]] / height
+
+    # The unsqueezed dim is the batch dim for each type
+    return {
+        "text_tokens": tokens,
+        "input_ids": tensorize(input_ids).unsqueeze(0),
+        "attention_mask": tensorize(input_mask).unsqueeze(0),
+        "token_type_ids": tensorize(segment_ids).unsqueeze(0),
+        "bbox": token_boxes,
+        # "actual_bbox": tensorize(token_actual_boxes).unsqueeze(0),
+        # "itc_labels": itc_labels.unsqueeze(0),
+        # "stc_labels": stc_labels.unsqueeze(0),
+        "labels": tensorize(labels).unsqueeze(0),
+        "are_box_first_tokens": tensorize(are_box_first_tokens).unsqueeze(0),
+    }
+
+
+def batch_parse_input(tokenizer, config, batch_data):
+    batch = []
+    text_tokens = []
+    for d in batch_data:
+        texts = d["text"]
+        actual_boxes = [poly_to_box(b) for b in d["coord"]]
+        image = Namespace(size=(d["img_sz"]["width"], d["img_sz"]["height"]))
+        label = d["label"]
+        fields = d["fields"]
+        features = parse_input(
+            image, texts, actual_boxes, tokenizer, config, label=label, fields=fields
+        )
+        text_tokens.append(features.pop("text_tokens"))
+        batch.append(features)
+
+    batch_features = {}
+    # for key in batch[0]:
+    #     print(key, batch[0][key].shape)
+    for key in batch[0]:
+        batch_features[key] = torch.cat([b[key] for b in batch], dim=0)
+
+    batch_features["text_tokens"] = text_tokens
+
+    return batch_features
+
+
+def hybrid_backbone(config_layoutlm, config_bert, layoutlm, bert, **kwargs):
+    bert = partially_from_pretrained(config_bert, bert, **kwargs)
+    layoutlm = partially_from_pretrained(config_layoutlm, layoutlm, **kwargs)
+    layoutlm.embeddings.word_embeddings = bert.embeddings.word_embeddings
+    layoutlm.embeddings.position_embeddings = bert.embeddings.position_embeddings
+    return layoutlm
+
+
+class BrosSpade(nn.Module):
+    def __init__(self, config_bert, fields, **kwargs):
+        super().__init__()
+        bert = config_bert._name_or_path
+        self.config_bert = config_bert
+        self.n_classes = n_classes = len(fields)
+        self.backbone = BrosModel.from_pretrained(
+            "naver-clova-ocr/bros-base-uncased",
+            local_files_only=True)
+        bert = AutoModel.from_pretrained(bert,
+                                         local_files_only=True)
+        self.backbone.embeddings.word_embeddings = bert.embeddings.word_embeddings
+        self.dropout = nn.Dropout(0.1)
+
+        self.rel_s = RelationTagger(
+            hidden_size=config_bert.hidden_size,
+            n_fields=self.n_classes,
+        )
+
+        self.rel_g = RelationTagger(
+            hidden_size=config_bert.hidden_size,
+            n_fields=self.n_classes,
+        )
+
+    def forward(self, batch):
+        batch = BatchEncoding(batch)
+        outputs = self.backbone(
+            input_ids=batch.input_ids,
+            bbox=batch.bbox,
+            attention_mask=batch.attention_mask,
+            # token_type_ids=batch.are_box_first_tokens,
+        )
+        last_hidden_state = outputs.last_hidden_state
+        rel_s = self.rel_s(self.dropout(last_hidden_state))
+        rel_g = self.rel_g(self.dropout(last_hidden_state))
+
+        input_masks = batch.are_box_first_tokens < 2
+        labels_s = batch.labels[:, 0].contiguous()
+        labels_g = batch.labels[:, 1].contiguous()
+        loss_s = self.spade_loss(rel_s, labels_s, input_masks)
+        loss_g = self.spade_loss(rel_g, labels_g, input_masks)
+
+        # true_rel_g = torch.softmax(rel_g, dim=1)[:, 0:1]
+        return Namespace(
+            rel=[rel_s, rel_g],
+            loss=[loss_s, loss_g],
+        )
+
+    def spade_loss(self, rel, labels, input_masks):
+        bsz, n, i, j = rel.shape
+        bsz, i1, j1 = labels.shape
+        assert i == i1
+        assert j == j1
+        lf = nn.CrossEntropyLoss(
+            weight=torch.tensor([0.1, 1], device=rel.device))
+        # lf = nn.CrossEntropyLoss()
+        true_lengths = true_length(input_masks)
+        loss = 0
+        labels = labels.type(torch.long)
+        for b in range(bsz):
+            nc = true_lengths[b]
+            nr = nc + self.n_classes
+            loss += lf(rel[b: b + 1, :, :nr, :nc], labels[b: b + 1, :nr, :nc])
+            # loss += lf(rel[b : b + 1], labels[b : b + 1])
+        return loss
+
+
+class SpadeDataset(Dataset):
+    def __init__(self, tokenizer, config, jsonl):
+        super().__init__()
+        with open(jsonl) as f:
+            data = [json.loads(line) for line in f.readlines()]
+
+        self.raw = data
+        self.fields = data[0]["fields"]
+        self.nfields = len(self.fields)
+        self._cached_length = len(data)
+        self.features = batch_parse_input(tokenizer, config, data)
+        self.text_tokens = self.features.pop("text_tokens")
+
+    def __len__(self):
+        return self._cached_length
+
+    def __getitem__(self, idx):
+        return BatchEncoding(
+            {key: self.features[key][idx] for key in self.features.keys()}
+        )
---
+++ train_bros_spade.py
@@ -0,0 +1,369 @@
+#!/usr/bin/env python
+# coding: utf-8
+from pprint import pprint
+from spade import graph_decoder
+import random
+from torch import nn
+from transformers import AutoModel, AutoTokenizer, AutoConfig, BatchEncoding
+from dataclasses import dataclass
+from typing import Optional
+from spade import model_bros as spade
+from spade.spade_inference import infer_single, post_process
+import numpy as np
+from pprint import pformat
+from random import randint
+import transformers
+import os
+import tqdm
+import json
+import torch
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import Dataset, DataLoader
+from spade.score import scores
+import sys
+
+writer = SummaryWriter()
+# from torch.nn.parallel import DistributedDataParallel as DDP
+
+
+def log_print(x):
+    with open("train.log", "a") as f:
+        f.write(str(x))
+        f.write("\n")
+        print(x)
+
+
+global BATCH_SIZE
+global CHECKPOINTDIR
+global NUM_HIDDEN_LAYERS
+global LAYOUTLM
+global BERT
+global train_data
+global test_data
+
+
+def vietnamese():
+    global BATCH_SIZE
+    global CHECKPOINTDIR
+    global NUM_HIDDEN_LAYERS
+    global LAYOUTLM
+    global BERT
+    global train_data
+    global test_data
+
+    BATCH_SIZE = 4
+    CHECKPOINTDIR = "checkpoint-bros-vnbill"
+    NUM_HIDDEN_LAYERS = 0
+    LAYOUTLM = "microsoft/layoutlm-base-cased"
+    BERT = "vinai/phobert-base"
+    train_data = "sample_data/train.jsonl"
+    test_data = "sample_data/test.jsonl"
+
+
+def vietnamese_cccd():
+    global BATCH_SIZE
+    global CHECKPOINTDIR
+    global NUM_HIDDEN_LAYERS
+    global LAYOUTLM
+    global BERT
+    global train_data
+    global test_data
+
+    BATCH_SIZE = 4
+    CHECKPOINTDIR = "checkpoint-bros-cccd"
+    NUM_HIDDEN_LAYERS = 0
+    LAYOUTLM = "microsoft/layoutlm-base-cased"
+    BERT = "vinai/phobert-base"
+    train_data = "sample_data/spade-data/CCCD/train.jsonl"
+    test_data = "sample_data/spade-data/CCCD/test.jsonl"
+
+
+def vietnamese_large():
+    global BATCH_SIZE
+    global CHECKPOINTDIR
+    global NUM_HIDDEN_LAYERS
+    global LAYOUTLM
+    global BERT
+    global train_data
+    global test_data
+
+    BATCH_SIZE = 1
+    CHECKPOINTDIR = "checkpoint-bros-vnbill-large"
+    NUM_HIDDEN_LAYERS = 10
+    LAYOUTLM = "microsoft/layoutlm-base-cased"
+    BERT = "vinai/phobert-large"
+    train_data = "sample_data/train.jsonl"
+    test_data = "sample_data/test.jsonl"
+
+
+def japanese():
+    global BATCH_SIZE
+    global CHECKPOINTDIR
+    global NUM_HIDDEN_LAYERS
+    global LAYOUTLM
+    global BERT
+    global train_data
+    global test_data
+
+    LAYOUTLM = "microsoft/layoutlm-base-cased"
+    # BERT = "bert-base-multilingual-cased"
+    BERT = "cl-tohoku/bert-base-japanese"
+    BATCH_SIZE = 3
+    CHECKPOINTDIR = "checkpoint-bros-jpcard"
+    NUM_HIDDEN_LAYERS = 0
+    train_data = "sample_data/spade-data/business_card/train.jsonl"
+    test_data = "sample_data/spade-data/business_card/test.jsonl"
+
+
+# vietnamese_large()
+vietnamese()
+# japanese()
+# vietnamese_cccd()
+LOAD_MODEL = False
+if NUM_HIDDEN_LAYERS > 0:
+    config_bert = AutoConfig.from_pretrained(
+        BERT, num_hidden_layers=NUM_HIDDEN_LAYERS)
+else:
+    config_bert = AutoConfig.from_pretrained(BERT)
+tokenizer = AutoTokenizer.from_pretrained(
+    BERT, local_files_only=False, **config_bert.to_dict()
+)
+config_layoutlm = AutoConfig.from_pretrained(
+    LAYOUTLM, local_files_only=True, **config_bert.to_dict()
+)
+
+
+dataset = spade.SpadeDataset(tokenizer, config_bert, train_data)
+test_dataset = spade.SpadeDataset(tokenizer, config_bert, test_data)
+
+
+dataloader = DataLoader(dataset, batch_size=BATCH_SIZE)
+dataloader_test = DataLoader(test_dataset, batch_size=BATCH_SIZE)
+
+print(dataset.nfields)
+model = spade.BrosSpade(
+    config_bert,
+    fields=dataset.fields,
+)
+print(model)
+# model = nn.DataParallel(model)
+
+
+# In[7]:
+
+
+def tail_collision_avoidance(adj, threshold=0.5, lmax=20):
+    # adj: m * n matrix
+    adj = torch.abs(adj.detach().clone())
+    m, n = adj.shape
+    old_size = n
+    pad_size = m - n
+    if pad_size > 0:
+        adj = torch.cat([adj, torch.zeros(m, pad_size)], dim=1)
+    m, n = adj.shape
+    assert m == n
+
+    def node_with_multiple_incoming(adj, threshold):
+        nodes = []
+        for i in range(n):
+            if torch.count_nonzero(adj[:, i] > threshold) > 1:
+                nodes.append(i)
+        return nodes
+
+    # Iterate in range
+    for _ in range(lmax):
+        nwmi = node_with_multiple_incoming(adj, threshold)
+        if len(nwmi) == 0:
+            break
+
+        trimmed_heads = []
+        for i in nwmi:
+            # j = highest linkink probability node
+            j = adj[:, i].argmax()
+            pr_ji = adj[j, i]
+
+            # trimmed head nodes
+            trimmed_heads.extend(
+                [k for k in range(n) if k != j and k !=
+                 i and adj[k, i] > threshold]
+            )
+
+            # trim
+            adj[:, i] = 0
+            adj[j, i] = 1
+
+        # find new tail nodes
+        trimmed_heads = list(set(trimmed_heads))
+        for i in trimmed_heads:
+            # top 3 nodes with highest probability
+            top3_val, top3_idx = torch.topk(adj[i, :], 3)
+            new_tails = [j for j in top3_idx if adj[i, j] >= threshold]
+            for j in new_tails:
+                adj[i, j] = 1
+    if pad_size > 0:
+        adj = adj[:, :old_size]
+    return adj
+
+
+# In[10]:
+
+
+# opt = torch.optim.Adam(model.parameters(), lr=5e-5)
+
+
+# In[11]:
+
+
+opt = torch.optim.AdamW(model.parameters(), lr=5e-5)
+lr_scheduler = transformers.get_cosine_schedule_with_warmup(
+    opt, num_warmup_steps=30, num_training_steps=1000
+)
+
+
+# In[ ]:
+
+
+# In[ ]:
+
+
+def auto_weight(x):
+    x = np.abs(x)
+    if x == 0:
+        return 1
+    return 10 ** (-np.round(np.log10(x)))
+
+
+try:
+    os.mkdir(CHECKPOINTDIR)
+except Exception:
+    pass
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = model.to(device)
+if LOAD_MODEL == True:
+    sd = torch.load(f"{CHECKPOINTDIR}/best_score.pt")
+    # sd = torch.load("./checkpoint/f1_s_max.pt")
+    print(model)
+    model.load_state_dict(sd, strict=False)
+
+min_loss = 9999
+max_score = 0
+loss_labels = ["loss_s", "loss_g"]
+s_bests = {}
+g_bests = {}
+for e in range(1000):
+    # if min_loss <= 3:
+    #     for p in model.backbone.parameters():
+    #         p.require_grad = True
+    # else:
+    #     for p in model.backbone.parameters():
+    #         p.require_grad = False
+    total_loss = [0 for _ in loss_labels]
+    summary_loss = 0
+    for batch_ in tqdm.tqdm(dataloader):
+        opt.zero_grad()
+        b = BatchEncoding(batch_).to(device)
+        model = model.to(device)
+        out = model(b)
+
+        if len(out.loss) > 1:
+            weight = [auto_weight(l.item()) for l in out.loss]
+            for i, l in enumerate(out.loss):
+                lv = l.item()
+                if lv == 0:
+                    print(f"Warning, loss {i} is zero")
+                total_loss[i] += lv
+                # l.backward(retain_graph=True)
+            # if sum(out.loss).item() > 10:
+            # loss = sum(out.loss)
+            # loss = out.loss[0] * 7 + out.loss[1] * 3
+            loss = sum(out.loss)
+            # else:
+            #     loss = sum([l * w for (l, w) in zip(out.loss, weight)])
+        else:
+            loss = out.loss[0]
+            total_loss[0] += out.loss[0].item()
+
+        summary_loss += loss.item()
+        loss.backward()
+        opt.step()
+        lr_scheduler.step()
+
+    if e % 20 == 0:
+        torch.save(model.state_dict(), f"{CHECKPOINTDIR}/model-%05d.pt" % e)
+    if loss < min_loss:
+        torch.save(model.state_dict(), f"{CHECKPOINTDIR}/model-least-loss.pt")
+        min_loss = loss.item()
+
+    try:
+        idx = randint(0, len(test_dataset))
+        test_batch = test_dataset[idx: idx + 1]
+        rel_s, rel_g = test_batch.labels[0, 0], test_batch.labels[0, 1]
+
+        data_id = dataset.raw[idx]["data_id"]
+        prediction, rel_s_pr, rel_g_pr = infer_single(
+            model, tokenizer, test_dataset, idx
+        )
+        ground_truth, _ = post_process(
+            tokenizer, rel_s, rel_g, test_batch, dataset.fields
+        )
+
+        print("---------------------------------")
+        print(data_id)
+        print("---------------------------------")
+        print("Predict:", prediction)
+        print("---------------------------------")
+        print("GTruth:", ground_truth)
+        print("---------------------------------")
+        writer.add_text("Inference/prediction", str(prediction), e)
+        writer.add_text("Inference/groud_truth", str(ground_truth), e)
+    except Exception:
+        import traceback
+
+        traceback.print_exc()
+        print(data_id)
+
+    for i in range(len(test_dataset)):
+        # dataset -> test_dataset...
+        classification, rel_s_pr, rel_g_pr = infer_single(
+            model, tokenizer, test_dataset, i
+        )
+        test_batch = test_dataset[i: i + 1]
+        rel_s_score_i = scores(test_batch.labels[0][0], rel_s_pr)
+        rel_g_score_i = scores(test_batch.labels[0][1], rel_g_pr)
+        if i == 0:
+            rel_s_score = rel_s_score_i
+            rel_g_score = rel_g_score_i
+        else:
+            for key, value in rel_s_score.items():
+                rel_s_score[key] += rel_s_score_i[key]
+                rel_g_score[key] += rel_g_score_i[key]
+
+    mean_score_s = rel_s_score
+    mean_score_g = rel_g_score
+    for key, value in mean_score_s.items():
+        mean_score_s[key] = mean_score_s[key] / len(test_dataset)
+        mean_score_g[key] = mean_score_g[key] / len(test_dataset)
+        pprint(f"Validation_s_edge_{key}: {mean_score_s[key]}")
+        pprint(f"Validation_g_edge_{key}: {mean_score_g[key]}")
+        writer.add_scalar(f"Score/s_{key}", mean_score_s[key], e)
+        writer.add_scalar(f"Score/g_{key}", mean_score_g[key], e)
+        if s_bests.get(key, 0) > mean_score_s[key]:
+            torch.save(model.state_dict(), f"{CHECKPOINTDIR}/s_best_{key}.pt")
+            s_bests[key] = mean_score_s[key]
+        if g_bests.get(key, 0) > mean_score_g[key]:
+            torch.save(model.state_dict(), f"{CHECKPOINTDIR}/g_best_{key}.pt")
+            g_bests[key] = mean_score_s[key]
+
+    current_score = mean_score_s["f1"] + mean_score_g["precision"]
+    if current_score > max_score:
+        max_score = current_score
+        torch.save(model.state_dict(), f"{CHECKPOINTDIR}/best_score.pt")
+    log_print(f"epoch {e + 1}")
+    nbatch = len(dataloader)
+    writer.add_scalar(f"Loss/total", summary_loss, e)
+    for (ll, lv) in zip(loss_labels, total_loss):
+        log_print(f"     {ll}: {lv}")
+        writer.add_scalar(f"Loss/{ll}", lv, e)
+    log_print(f"     total: {summary_loss}")
+    log_print(f"     best score: {max_score}")
