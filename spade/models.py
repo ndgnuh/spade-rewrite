261b3589919efafda7be061b1706a99ee91ac2a3
Nguyễn Đức Hùng <hungnd@hiworld.com.vn>
2022-04-13 16:29:23 +0700

    Spade loss function

--- spade/models.py
+++ spade/models.py
@@ -27,3 +27,38 @@ class RelationTagger(nn.Module):
             enc_head, self.W_label_1(enc_tail).transpose(1, 2))
 
         return torch.cat([score_0.unsqueeze(1), score_1.unsqueeze(1)], dim=1)
+
+
+class SpadeLoss(nn.Module):
+    def __init__(self, num_fields, weight=[0.1, 1]):
+        super().__init__()
+        weight = torch.tensor(weight)
+        self.loss = nn.CrossEntropyLoss(weight=weight)
+        self.num_fields = num_fields
+
+    def true_length(self, mask):
+        batch = len(mask.shape) == 2
+        if batch:
+            return [self.true_length(m) for m in mask]
+        return torch.where(mask == 1)[-1][-1]
+
+    def forward(self, relations, labels, input_masks=None):
+        bsz, n, i, j = relations.shape
+        bsz, i1, j1 = labels.shape
+        assert i == i1
+        assert j == j1
+        # lf = nn.CrossEntropyLoss()
+        if input_masks is not None:
+            true_lengths = self.true_length(input_masks)
+        else:
+            true_lengths = [j for _ in range(bsz)]
+        loss = 0
+        labels = labels.type(torch.long)
+        for b in range(bsz):
+            nc = true_lengths[b]
+            nr = nc + self.num_fields
+            loss += self.loss(relations[b: b + 1, :, :nr, :nc],
+                              labels[b: b + 1, :nr, :nc])
+            # loss += lf(rel[b : b + 1], labels[b : b + 1])
+        return loss
+        pass
