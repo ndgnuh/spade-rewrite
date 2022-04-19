4a6e5edf0c2205cd040a07e5e8463cb6f0411530
Hung Nguyen <hungnd@hiworld.com.vn>
2022-04-19 11:08:00 +0700

    input transformation/google api

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
 import spade.models as models
 import streamlit as st
+import json
+from google.cloud import vision
+import os
+import spade.transforms as transforms
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] =\
+    '/home/hung/grooo-gkeys.json'
 
 st.header("Trích xuất hóa đơn")
 
+
+def ocr(content: bytes):
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image(content=content)
+    response = client.text_detection(image=image)
+    return json.loads(type(response).to_json(response))
+
+
 with st.spinner(text="Loading tokenizer"):
-    tokenizer = st.cache(models.AutoTokenizer)(
+    tokenizer = st.experimental_singleton(models.AutoTokenizer)(
         "vinai/phobert-base", local_files_only=True)
     st.success("Tokenizer loaded")
 
 upload_methods = ["Từ thư viện trong máy", "Chụp ảnh mới"]
-upload_method = st.selectbox("Phương pháp upload ảnh", upload_methods)
+upload_method = st.radio("Phương pháp upload ảnh", upload_methods)
 
 
 if upload_methods.index(upload_method) == 0:
     image = st.file_uploader("Upload file")
 else:
     image = st.camera_input("Chụp ảnh")
-print(upload_method)
 
-c1, c2 = st.columns((1, 1))
-with c1:
+if image is not None:
+    st.image(image)
     submit = st.button("Nhận dạng")
-clear = c2.button("clear")
+    clear = st.button("clear")
+else:
+    submit = clear = False
+
+if submit:
+    with st.spinner(text="OCR..."):
+        res = ocr(image.getvalue())
+        input_data = transforms.from_google(res)
+        print(input_data)
+    # with open("response.json", 'w') as f:
+    #     f.write(json.dumps(res, ensure_ascii=False, indent=2))
+    # st.write(json.dumps(res, ensure_ascii=False, indent=2))
---
+++ spade/transforms.py
@@ -0,0 +1,33 @@
+from lenses import lens
+from .box import Box
+from .models import SpadeData
+
+
+def from_google(response):
+    def box_from_google_api_poly(poly):
+        #   [{'x': 25, 'y': 446},
+        #    {'x': 549, 'y': 446},
+        #    {'x': 549, 'y': 1234},
+        #    {'x': 25, 'y': 1234}]
+        x = [p['x'] for p in poly]
+        y = [p['y'] for p in poly]
+        return Box(x + y, Box.Type.X4Y4)
+
+    boxes_focus = lens['textAnnotations'].Each()['boundingPoly']['vertices'].F(
+        box_from_google_api_poly).collect()
+    texts_focus = lens['textAnnotations'].Each()['description'].collect()
+
+    boxes = boxes_focus(response)
+    x0, y0, w, h = boxes[0].xywh
+
+    def normalize(b):
+        x = [max(x - x0, 0) for x in b.x4y4[:4]]
+        y = [max(y - y0, 0) for y in b.x4y4[4:]]
+        return Box(x + y, Box.Type.X4Y4)
+    boxes = [normalize(b) for b in boxes]
+
+    return SpadeData(texts=texts_focus(response)[1:],
+                     boxes=boxes[1:],
+                     width=w,
+                     height=h,
+                     relations=None)
