986efc99166e17ceccad8103392b2f4fa2179775
Hung Nguyen <hungnd@hiworld.com.vn>
2022-04-20 09:50:45 +0700

    Add model

--- spade/models.py
+++ spade/models.py
@@ -2,9 +2,12 @@ import torch
 from torch import nn
 from dataclasses import dataclass
 from typing import Optional, List, Dict
-from .box import Box
+from argparse import Namespace
 from functools import cache
-from transformers import AutoTokenizer as AutoTokenizer_
+from transformers import AutoTokenizer as AutoTokenizer_, BatchEncoding, AutoModel
+from .graph_utils import expand_relation
+from .bros.bros import BrosModel
+from .box import Box
 
 
 @dataclass
@@ -66,6 +69,22 @@ class RelationTagger(nn.Module):
 
 
 class SpadeLoss(nn.Module):
+    """Spade Loss function
+
+    it's basically cross entropy but weighted, the default weight is 
+    [0.1, 1], which bias the "has edge" channel.
+
+    Parameters
+    ----------
+    relations: torch.Tensor
+        logits of shape batch * n_rel * (fields + seq) * seq
+    labels: torch.Tensor
+        ground truth relation of shape batch * (fields + seq) * seq
+    input_masks: Optinal[torch.Tensor]
+        the mask for true input (input wihtout padding), if this is None
+        the loss will also includes padding loss.
+    """
+
     def __init__(self, num_fields, weight=[0.1, 1]):
         super().__init__()
         weight = torch.tensor(weight)
@@ -95,7 +114,6 @@ class SpadeLoss(nn.Module):
             nr = nc + self.num_fields
             loss += self.loss(relations[b: b + 1, :, :nr, :nc],
                               labels[b: b + 1, :nr, :nc])
-            # loss += lf(rel[b : b + 1], labels[b : b + 1])
         return loss
 
 
@@ -108,10 +126,190 @@ def AutoTokenizer(*args, **kwargs):
     return AutoTokenizer_.from_pretrained(*args, **kwargs)
 
 
+def map_token(tokenizer, texts: List, offset: int = 0) -> List[List[int]]:
+    """Map word index to token index
+
+    Parameters
+    ------------
+    tokenizer:
+        transformers tokenizer
+    texts: List[str]
+        list of texts
+    offset: int = 0
+        label offset
+    """
+    map = [[i] for i in range(offset)] + [[] for _ in texts]
+
+    current_node_idx = offset
+    for (i, text) in enumerate(texts):
+        tokens = tokenizer.tokenize(text)
+        for token in tokens:
+            map[i + offset].append(current_node_idx)
+            current_node_idx += 1
+    return map
+
+
 def preprocess(config: Dict,
                data: SpadeData):
-    #
+    """Preprocess data before put inside spade
+
+    Parameters:
+    ----------
+    config: dict
+        Configuration, must have the keys:
+        - tokenizer: str (tokenizer name)
+        - max_position_embeddings: int
+        - bbox_type: Box.Type
+        - relation_expansions: (unused for now)
+    data: SpadeData
+        Data to be processed
+    """
+    # Context
     texts = data.texts
     boxes = data.boxes
     width = data.width
     height = data.height
+    relations = data.relations
+    tokenizer = AutoTokenizer(config['tokenizer'])
+
+    boxes = [b.normalize(width, height) for b in boxes]
+    # token_map = G.map_token(tokenizer, texts, offset=len(config['num_fields']))
+
+    #== TOKENIZE ==#
+    tokens = []
+    token_boxes = []
+    token_types = []
+    for text, box in zip(texts, boxes):
+        text_tokens = tokenizer.tokenize(text)
+        num_tokens = len(text_tokens)
+        tokens.extend(text_tokens)
+        token_boxes.extend([box] * num_tokens)
+        token_types.extend([1] + [0] * (num_tokens - 1))
+    input_masks = [1] * len(tokens)
+
+    if relations is not None:
+        # PROBLEM:
+        # Each relation has their own way of expanding the graph
+        # so we define some extra input here
+        expansions = config['relation_expansions']
+        for i, relation in enumerate(relations):
+            relations[i] = \
+                expand_relation(relation, token_map, **expansions[i])
+
+    # PADDING
+    special_tokens_count = 2
+    true_length = config['max_position_embeddings'] - special_tokens_count
+    if len(tokens) > true_length:
+        tokens = tokens[:true_length]
+        token_boxes = token_boxes[:true_length]
+        token_types = token_types[:true_length]
+        input_masks = token_types[:true_length]
+
+    # SEP TOKEN
+    tokens += [tokenizer.sep_token]
+    input_masks += [1]
+    token_boxes += [Box.sep_token(width, height)]
+    token_types += [1]
+
+    # CLS token
+    tokens = [tokenizer.cls_token] + tokens
+    token_boxes = [Box.cls_token()] + token_boxes
+    input_masks = input_masks + [0]
+    token_types = [2] + token_types
+
+    # PAD TOKEN
+    padding_length = config['max_position_embeddings'] - len(tokens)
+    tokens += [tokenizer.pad_token] * padding_length
+    input_masks += [0] * padding_length
+    token_boxes += [Box.pad_token()] * padding_length
+    token_types += [3] * padding_length
+
+    # BOUNDING BOX FORMAT
+    token_boxes = [getattr(b, config['bbox_type']) for b in token_boxes]
+
+    # INPUT IDS
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    def tensorize(x):
+        return torch.tensor((x))
+
+    return BatchEncoding({
+        "input_ids": tensorize(input_ids).unsqueeze(0),
+        "attention_mask": tensorize(input_masks).unsqueeze(0),
+        "token_type_ids": tensorize(token_types).unsqueeze(0),
+        "bbox": tensorize(token_boxes).unsqueeze(0),
+    })
+
+
+class BrosSpade(nn.Module):
+    """Model include clovaai/BROS backbone + clovaai/SPADE head.
+    """
+
+    def __init__(self, config, **kwargs):
+        """
+        WARNING: this doc is written as-is, the code doesn't follow this one (yet)
+        Paramters
+        ----------
+        config: Dict
+            the model configuration, see extra configuration keys below,
+            other configuration will derive from model config on huggingface
+
+        Configuration keys
+        ----------
+        backbone: str
+            Backbone name (for language hybrid layers)
+        bros_backbone: str
+            BROS backbone name (default: "naver-clova-ocr/bros-base-uncased")
+        local_files_only: bool
+            Whether to query hugging face server (default: False)
+        hidden_dropout_prob: float
+            Hidden dropout probability (default: 0.1)
+        """
+        super().__init__()
+        bert = config_bert._name_or_path
+        self.config_bert = config_bert
+        self.n_classes = n_classes = len(fields)
+        self.backbone = BrosModel.from_pretrained(
+            "naver-clova-ocr/bros-base-uncased",
+            local_files_only=True)
+        bert = AutoModel.from_pretrained(bert,
+                                         local_files_only=True)
+        self.backbone.embeddings.word_embeddings = bert.embeddings.word_embeddings
+        self.dropout = nn.Dropout(0.1)
+
+        self.rel_s = RelationTagger(
+            hidden_size=config_bert.hidden_size,
+            n_fields=self.n_classes,
+        )
+
+        self.rel_g = RelationTagger(
+            hidden_size=config_bert.hidden_size,
+            n_fields=self.n_classes,
+        )
+        self.loss = SpadeLoss(num_fields=n_fields)
+
+    def forward(self, batch):
+        batch = BatchEncoding(batch)
+        outputs = self.backbone(
+            input_ids=batch.input_ids,
+            bbox=batch.bbox,
+            attention_mask=batch.attention_mask,
+        )
+        last_hidden_state = outputs.last_hidden_state
+        rel_s = self.rel_s(self.dropout(last_hidden_state))
+        rel_g = self.rel_g(self.dropout(last_hidden_state))
+
+        if 'labels' in batch:
+            input_masks = batch.token_types_ids < 2
+            labels_s = batch.labels[:, 0].contiguous()
+            labels_g = batch.labels[:, 1].contiguous()
+            loss_s = self.loss(rel_s, labels_s, input_masks)
+            loss_g = self.loss(rel_g, labels_g, input_masks)
+        else:
+            loss_s = loss_g = None
+
+        # true_rel_g = torch.softmax(rel_g, dim=1)[:, 0:1]
+        return Namespace(
+            rel=[rel_s, rel_g],
+            loss=[loss_s, loss_g],
+        )
