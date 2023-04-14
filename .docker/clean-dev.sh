38be84b247831b31610abfec9360a9e41aff5d89
Hung Nguyen <ndgnuh@protonmail.com>
2023-04-14 10:22:37 +0700

    remove dev env, use dirty torch instead

--- .docker/clean-dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker-compose -f .docker/docker-compose.yml stop
-docker rm -f spade-dev
--- .docker/enter-dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker-compose -f .docker/docker-compose.yml up -d
-docker exec -it -e HOME=/tmp -u$(id -u $USER) $@ spade-dev bash
